20737729514a456dca694180ea29975170231ec3
poiz1245 <wva960@gmail.com>
2024-05-28 10:06:29 +0900

    에셋 임포트 후 캐릭터 변경

--- VR_ExperienceCenter/Assets/Scripts/VRRig.cs
+++ VR_ExperienceCenter/Assets/Scripts/VRRig.cs
@@ -1,35 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
 public class VRMap
 {
     public Transform vrTarget;
-    public Transform ikTarget;
+    public Transform rigTarget;
     public Vector3 trackingPositionOffset;
     public Vector3 trackingRotationOffset;
+
     public void Map()
     {
-        ikTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
-        ikTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+        rigTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+        rigTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
     }
 }
 
 public class VRRig : MonoBehaviour
 {
-    [Range(0, 1)]
-    public float turnSmoothness = 0.1f;
     public VRMap head;
     public VRMap leftHand;
     public VRMap rightHand;
 
-    public Vector3 headBodyPositionOffset;
-    public float headBodyYawOffset;
+    public Transform headConstraint;
+    public Vector3 headBodyOffset;
 
-    void LateUpdate()
+    private void Start()
     {
-        transform.position = head.ikTarget.position + headBodyPositionOffset;
-        float yaw = head.vrTarget.eulerAngles.y;
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(transform.eulerAngles.x, yaw, transform.eulerAngles.z), turnSmoothness);
+        headBodyOffset = transform.position - headConstraint.position;
+    }
+
+    private void Update()
+    {
+        transform.position = headConstraint.position + headBodyOffset;
+        transform.forward = Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized; //머리 회전을 y축으로만 하도록 허용
 
         head.Map();
         leftHand.Map();
