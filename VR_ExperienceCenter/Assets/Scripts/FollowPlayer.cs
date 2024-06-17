d0f6340d63a4d87f5add7f467c581c8c74578d99
poiz1245 <wva960@gmail.com>
2024-06-17 17:48:07 +0900

    feat : 오브젝트 그랩 시 움직임 스크립트 생성
    - 그랩 할 때 그 자리유지
    - 그랩 할 때 거리 유지하면서 플레이어 카메라 중앙에 맞게 쫓아옴

---
+++ VR_ExperienceCenter/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+    [SerializeField] RightController rightController;
+    [SerializeField] XRGrabInteractable grabInteractable;
+
+    bool isGrab = false;
+    float initialDistance;
+    Vector3 initialDirection;
+    Quaternion initialRotation;
+    MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material.SetInt("_ZWrite", 0);
+        meshRenderer.material.SetInt("_ZTest", (int)UnityEngine.Rendering.CompareFunction.Always);
+    }
+    void Update()
+    {
+        if (grabInteractable.isSelected && !isGrab)
+        {
+            initialDistance = Vector3.Distance(transform.position, mainCamera.transform.position);
+            //initialDirection = (mainCamera.transform.position - transform.position).normalized;
+            initialRotation = mainCamera.transform.rotation;
+            isGrab = true;
+        }
+        else if (grabInteractable.isSelected && isGrab)
+        {
+            //transform.position = mainCamera.transform.position - (initialDirection * initialDistance);
+            //transform.rotation = Quaternion.LookRotation(mainCamera.transform.position - transform.position);
+            transform.position = mainCamera.transform.position + (mainCamera.transform.forward * initialDistance);
+            transform.rotation = initialRotation;
+        }
+        else if(!grabInteractable.isSelected && isGrab)
+        {
+            isGrab = false;
+        }
+
+        
+    }
+}
