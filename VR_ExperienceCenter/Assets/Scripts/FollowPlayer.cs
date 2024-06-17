c14853c2f99948c19f28bf708ae7054823b8db8a
poiz1245 <wva960@gmail.com>
2024-06-17 17:54:16 +0900

    Test : fristrendering

--- VR_ExperienceCenter/Assets/Scripts/FollowPlayer.cs
+++ VR_ExperienceCenter/Assets/Scripts/FollowPlayer.cs
@@ -13,16 +13,11 @@ public class FollowPlayer : MonoBehaviour
     float initialDistance;
     Vector3 initialDirection;
     Quaternion initialRotation;
-    MeshRenderer meshRenderer;
 
-    private void Start()
-    {
-        meshRenderer = GetComponent<MeshRenderer>();
-        meshRenderer.material.SetInt("_ZWrite", 0);
-        meshRenderer.material.SetInt("_ZTest", (int)UnityEngine.Rendering.CompareFunction.Always);
-    }
     void Update()
     {
+        GetComponent<Renderer>().sortingOrder = int.MaxValue;
+
         if (grabInteractable.isSelected && !isGrab)
         {
             initialDistance = Vector3.Distance(transform.position, mainCamera.transform.position);
@@ -37,11 +32,11 @@ public class FollowPlayer : MonoBehaviour
             transform.position = mainCamera.transform.position + (mainCamera.transform.forward * initialDistance);
             transform.rotation = initialRotation;
         }
-        else if(!grabInteractable.isSelected && isGrab)
+        else if (!grabInteractable.isSelected && isGrab)
         {
             isGrab = false;
         }
 
-        
+
     }
 }
