fa085e74392f8c8afa370e29973b7f58293b8c3a
poiz1245 <wva960@gmail.com>
2024-06-17 17:47:21 +0900

    feat : 안쓰는 코드 삭제

--- VR_ExperienceCenter/Assets/Scripts/RightController.cs
+++ VR_ExperienceCenter/Assets/Scripts/RightController.cs
@@ -8,25 +8,17 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class RightController : MonoBehaviour
 {
     [SerializeField] InputActionReference grab;
-    [SerializeField] XRGrabInteractable interactable;
-    [SerializeField] Animator anim;
 
     public GameObject rightController;
     public Vector3 rightControllerPosition;
 
-    private void Update()
+    private void Start()
     {
         grab.action.performed += ObjectGrab;
 
-       /* if (!interactable.isSelected)
-        {
-            anim.SetBool("isGrab", false);
-        }*/
     }
-
     public void ObjectGrab(InputAction.CallbackContext obj)
     {
         rightControllerPosition = rightController.transform.position;
-        //anim.SetBool("isGrab", true);
     }
 }
