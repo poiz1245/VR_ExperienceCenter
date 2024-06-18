856cc537eb3ea600f18a4e4b03c941774e7420c8
poiz1245 <wva960@gmail.com>
2024-06-18 11:04:24 +0900

    feat : anamorphicillusion을 활용하여 오브젝트 생성 구현

--- VR_ExperienceCenter/Assets/Scripts/AnamorphicillusionEffect.cs
+++ VR_ExperienceCenter/Assets/Scripts/AnamorphicillusionEffect.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class AnamorphicillusionEffect : MonoBehaviour
+public class IllusionEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Camera mainCamera;
+    public Transform targetObject;
+    public GameObject[] scatteredImages;
+    public GameObject finalObject;
+    public Camera anamorphicCamera;
 
-    // Update is called once per frame
+    public float acceptableAngle;
+    public float acceptableDistance;
+
     void Update()
     {
-        
+        Vector3 directionToTarget = targetObject.position - mainCamera.transform.position;
+        float angle = Vector3.Angle(mainCamera.transform.forward, directionToTarget);
+        float distance = Vector3.Distance(mainCamera.transform.position, anamorphicCamera.transform.position);
+
+        if (angle < acceptableAngle && distance < acceptableDistance)
+        {
+            foreach (GameObject image in scatteredImages)
+            {
+                image.SetActive(false);
+            }
+            finalObject.SetActive(true);
+        }
     }
+
 }
