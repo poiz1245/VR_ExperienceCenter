4c7d5ae31928b7b68e3634c33a527c3fc084f716
poiz1245 <wva960@gmail.com>
2024-06-24 17:25:55 +0900

    feat : 스텐실 룸 포탈 통과 시 오브젝트 레이어 변경하여 실체화

---
+++ VR_ExperienceCenter/Assets/1. Scripts/Portal.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject stencilRoom;
+    int changeVisibleLayer;
+    int changeInVisibleLayer;
+
+    bool walkIn = false;
+    private void Start()
+    {
+        changeVisibleLayer = LayerMask.NameToLayer("Default");
+        changeInVisibleLayer = LayerMask.NameToLayer("Stencil6");
+    }
+    void ChangeLayerRecursively(GameObject obj, int layer)
+    {
+        obj.layer = layer;
+        // 현재 오브젝트는 변경하지 않고, 자식 오브젝트만 변경
+        for (int i = 0; i < obj.transform.childCount; i++)
+        {
+            ChangeLayerRecursively(obj.transform.GetChild(i).gameObject, layer);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (!walkIn)
+            {
+                walkIn = true;
+                ChangeLayerRecursively(stencilRoom, changeVisibleLayer);
+            }
+            else
+            {
+                walkIn = false;
+                ChangeLayerRecursively(stencilRoom, changeInVisibleLayer);
+            }
+        }
+    }
+
+}
