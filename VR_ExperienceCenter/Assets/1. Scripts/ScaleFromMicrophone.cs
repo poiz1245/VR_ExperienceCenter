b6239e5e2e01a324fd1c96d670ea9adb97ef0a3b
poiz1245 <wva960@gmail.com>
2024-06-25 16:41:07 +0900

    feat : 소리 slider에 전달

--- VR_ExperienceCenter/Assets/1. Scripts/ScaleFromMicrophone.cs
+++ VR_ExperienceCenter/Assets/1. Scripts/ScaleFromMicrophone.cs
@@ -1,24 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScaleFromMicrophone : MonoBehaviour
 {
-    public AudioSource source;
-    public Vector3 minScale;
-    public Vector3 maxScale;
+    //public AudioSource source;
+    /*public Vector3 minScale;
+    public Vector3 maxScale;*/
+    [SerializeField] float minValue;
+    [SerializeField] float maxValue;
     public AudioLoudnessDetection detector;
+
+    Slider slider;
 
     public float loudnessSensibility = 100;
     public float threshold = 0.1f;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+        slider = GetComponent<Slider>();
     }
-
-    // Update is called once per frame
     void Update()
     {
         float loudness = detector.GetLoudnessFromMicrophone() * loudnessSensibility;
@@ -26,6 +28,10 @@ public class ScaleFromMicrophone : MonoBehaviour
         if (loudness < threshold)
             loudness = 0;
 
-        transform.localScale = Vector3.Lerp(minScale, maxScale, loudness);
+        slider.value = loudness;
+
+        print(loudness);
+
+        //transform.localScale = Vector3.Lerp(minScale, maxScale, loudness);
     }
 }
