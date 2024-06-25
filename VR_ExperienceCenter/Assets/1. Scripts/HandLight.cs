bde81c2c0f964b367c2dec00326723f815395ac6
poiz1245 <wva960@gmail.com>
2024-06-25 09:13:32 +0900

    feat : 손전등을 잡고 있을 때만 모드 변경 가능

--- VR_ExperienceCenter/Assets/1. Scripts/HandLight.cs
+++ VR_ExperienceCenter/Assets/1. Scripts/HandLight.cs
@@ -3,34 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class HandLight : MonoBehaviour
 {
     [SerializeField] InputActionReference triggerButton;
     [SerializeField] Light handLight;
+
+    XRGrabInteractable interactable;
     bool focusOn = false;
 
     void Start()
     {
         triggerButton.action.performed += HandLightFocusOn;
         handLight.type = UnityEngine.LightType.Spot;
+        interactable = GetComponent<XRGrabInteractable>();
     }
 
     void HandLightFocusOn(InputAction.CallbackContext context)
     {
-        if (!focusOn)
+        if(interactable.isSelected)
         {
-            handLight.intensity = 100f;
-            handLight.innerSpotAngle = 10f;
-            handLight.spotAngle = 60f;
-            focusOn = true;
+            if(!focusOn)
+            {
+                handLight.intensity = 200f;
+                handLight.innerSpotAngle = 20f;
+                handLight.spotAngle = 30f;
+                focusOn = true;
+            }
+            else if (focusOn)
+            {
+                handLight.intensity = 100f;
+                handLight.innerSpotAngle = 10f;
+                handLight.spotAngle = 60f;
+                focusOn = false;
+            }
+            
         }
-        else
-        {
-            handLight.intensity = 200f;
-            handLight.innerSpotAngle = 20f;
-            handLight.spotAngle = 30f;
-            focusOn = false;
-        }
+       
     }
 }
