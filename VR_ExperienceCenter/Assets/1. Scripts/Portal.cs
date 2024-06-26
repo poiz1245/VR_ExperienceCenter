544b2bec402b512048cd521a9ed47a5453039849
poiz1245 <wva960@gmail.com>
2024-06-26 17:37:54 +0900

    feat : 포탈 기능 수정

--- VR_ExperienceCenter/Assets/1. Scripts/Portal.cs
+++ VR_ExperienceCenter/Assets/1. Scripts/Portal.cs
@@ -1,24 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Portal : MonoBehaviour
 {
     [SerializeField] GameObject stencilRoom;
     [SerializeField] Collider[] childrenCollider;
+
     //BoxCollider collider;
+
+    Rigidbody rigid;
+    Collider collider;
+    XRGrabInteractable interactable;
+    Portal portal;
 
     int changeVisibleLayer;
     int changeInVisibleLayer;
 
+    bool isGrabed = false;
     public bool walkIn { get; private set; }
     private void Start()
     {
+        collider = GetComponent<MeshCollider>();
+        rigid = GetComponent<Rigidbody>();
+        interactable = GetComponent<XRGrabInteractable>();
         changeVisibleLayer = LayerMask.NameToLayer("Default");
         changeInVisibleLayer = LayerMask.NameToLayer("Stencil5");
         walkIn = false;
     }
-    void ChangeLayerRecursively(GameObject obj, int layer)
+
+    private void Update()
+    {
+        if (interactable.isSelected)
+        {
+            rigid.constraints &= ~RigidbodyConstraints.FreezePositionY;
+        }
+    }
+    void ChangeLayerRecursively(GameObject obj, int layer)  // 포탈의 하위 오브젝트들의 레이어를 변경하는 메서드
     {
         obj.layer = layer;
         // 현재 오브젝트는 변경하지 않고, 자식 오브젝트만 변경
@@ -36,8 +56,8 @@ public class Portal : MonoBehaviour
         {
             obj.SetActive(false);
         }
-    }
-    bool IsChildOfPlayerObject(GameObject obj)
+    } // 플레이어와 그 하위 오브젝트 제외하고 모두 비활성화하는 메서드
+    bool IsChildOfPlayerObject(GameObject obj) //Player오브젝트와 그 하위 모든 오브젝트 찾는 메서드
     {
         if (obj.CompareTag("Player"))
         {
@@ -45,7 +65,7 @@ public class Portal : MonoBehaviour
         }
 
         Transform parent = obj.transform.parent;
-        while(parent != null)
+        while (parent != null)
         {
             if (parent.gameObject.CompareTag("Player"))
             {
@@ -55,10 +75,20 @@ public class Portal : MonoBehaviour
         }
         return false;
     }
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collider.isTrigger = true;
+        }
+
+        
+    }*/
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            print("충돌");
             if (!walkIn)
             {
                 walkIn = true;
@@ -75,12 +105,22 @@ public class Portal : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player") && walkIn)
         {
+            rigid.useGravity = false;
             for (int i = 0; i < childrenCollider.Length; i++)
             {
-                childrenCollider[i].isTrigger = false;
+                childrenCollider[i].enabled = true;
             }
 
             DisableAllExcludedObjects();
+
+            interactable.enabled = false;
+
+            if (portal != null)
+            {
+                portal.enabled = false;
+            }
+
+
         }
     }
 
