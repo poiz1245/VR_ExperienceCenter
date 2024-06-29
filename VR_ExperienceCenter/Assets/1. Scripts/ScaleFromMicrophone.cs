aeaec9df3966672bf632e5a1c7c709d25b4e46c2
poiz1245 <wva960@gmail.com>
2024-06-30 03:21:47 +0900

    feat : 플레이어 소리 감지하는 이벤트 생성

--- VR_ExperienceCenter/Assets/1. Scripts/ScaleFromMicrophone.cs
+++ VR_ExperienceCenter/Assets/1. Scripts/ScaleFromMicrophone.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Cinemachine.DocumentationSortingAttribute;
+using static ScaleFromMicrophone;
 
 public class ScaleFromMicrophone : MonoBehaviour
 {
@@ -9,45 +11,43 @@ public class ScaleFromMicrophone : MonoBehaviour
     [SerializeField] Vector3 minScale;
     [SerializeField] Vector3 maxScale;
     [SerializeField] AudioLoudnessDetection detector;
+    [SerializeField] GameObject warningImage;
 
     public float loudnessSensibility = 100;
     public float threshold = 0.1f;
 
     float loudness;
-    private void Start()
-    {
-    }
+        
+    public delegate void SoundVolumeChanged(float loudness);
+    public event SoundVolumeChanged OnSoundVolumeChanged;
+
     void Update()
     {
-        //loudness = GetAveragedVolume() * loudnessSensibility; 오디오 소스 활용할 때 이렇게 쓰는듯
-
-
-        //loudness = detector.GetLoudnessFromMicrophone() * loudnessSensibility;
-
-        loudness = source.volume;
+        loudness = detector.GetLoudnessFromMicrophone() * loudnessSensibility;
 
         if (loudness < threshold)
             loudness = 0;
 
+        OnSoundVolumeChanged?.Invoke(loudness);
+        warningImage.transform.localScale = Vector3.Lerp(minScale, maxScale, loudness);
 
-        transform.localScale = Vector3.Lerp(minScale, maxScale, loudness);
+
+        //loudness = GetAveragedVolume() * loudnessSensibility; 오디오 소스 활용할 때 이렇게 쓰는듯
+        //loudness = source.volume;
     }
-    void GetThumbStickValue()
-    {
 
-
-    }
+   
     /*float GetAveragedVolume() //오디오 소스에서 나오는 데이터를 수치화 하는 함수인듯
-    {
-        float[] data = new float[256];
-        float a = 0;
-        source.GetOutputData(data, 0);
-        foreach (float s in data)
-        {
-            a += Mathf.Abs(s);
-        }
-        return a / 256;
-    }*/
+{
+   float[] data = new float[256];
+   float a = 0;
+   source.GetOutputData(data, 0);
+   foreach (float s in data)
+   {
+       a += Mathf.Abs(s);
+   }
+   return a / 256;
+}*/
 
 
 }
