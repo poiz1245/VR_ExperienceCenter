837c3a67991b79390e43a9fe6a85880762618ea0
poiz1245 <wva960@gmail.com>
2024-07-02 13:47:17 +0900

    feat : 왼손 컨트롤러 메뉴 버튼으로 restart 기능 구현

--- VR_ExperienceCenter/Assets/1. Scripts/Menu.cs
+++ VR_ExperienceCenter/Assets/1. Scripts/Menu.cs
@@ -2,18 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public InputActionReference menuButton;
-    // Start is called before the first frame update
+    public InputActionReference menu;
+    [SerializeField] GameObject menuButton;
+
+    bool menuActive = false;
+    public enum SceneName
+    {
+        Main,
+        Stage4,
+        Stage5,
+        Chase
+    }
+
+    public SceneName sceneName;
+
     void Start()
     {
-        menuButton.action.performed += OnClickMenu;
+        menu.action.performed += OnClickMenu;
     }
 
     void OnClickMenu(InputAction.CallbackContext context)
     {
+        if (!menuActive)
+        {
+            menuActive = true;
+            menuButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            menuActive = false;
+            menuButton.gameObject.SetActive(false);
+        }
+    }
 
+    public void OnClickMenuButton()
+    {
+        SceneManager.LoadScene(sceneName.ToString());
     }
 }
