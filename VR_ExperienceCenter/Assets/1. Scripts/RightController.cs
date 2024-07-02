8dc132f12c535cd4bae127fc3dd6b4442d40509f
poiz1245 <wva960@gmail.com>
2024-07-03 00:10:59 +0900

    build setting

--- VR_ExperienceCenter/Assets/1. Scripts/RightController.cs
+++ VR_ExperienceCenter/Assets/1. Scripts/RightController.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.XR.Interaction.Toolkit;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
