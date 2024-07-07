1017747d78ea0e9311e29ca99652404a3707e7b0
poiz1245 <wva960@gmail.com>
2024-07-07 21:27:34 +0900

    Stage6 장애물 및 함정 사운드 설치

--- VR_ExperienceCenter/Assets/1. Scripts/JumpScaredTrapTrigger.cs
+++ VR_ExperienceCenter/Assets/1. Scripts/JumpScaredTrapTrigger.cs
@@ -9,19 +9,26 @@ public class JumpScaredTrapTrigger : MonoBehaviour
     [SerializeField] Vector3 move;
     [SerializeField] Vector3 rotation;
     [SerializeField] float duration;
+    [SerializeField] DOTweenAnimation light;
 
     [SerializeField] AudioSource audioSource;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            if (trapObject != null) 
+            if (trapObject != null)
             {
                 trapObject.transform.DOMove(move, duration).SetEase(Ease.Linear);
                 trapObject.transform.DORotate(rotation, duration).SetEase(Ease.Linear);
+
+                if (light != null)
+                {
+                    light.DOPlay();  
+                }
             }
-            
-            if(audioSource != null)
+
+            if (audioSource != null)
             {
                 audioSource.Play();
             }
